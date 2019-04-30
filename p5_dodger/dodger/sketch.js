2a0f0adb5fe05fcb4440d4094ab856207e5fe587
Kevin Gage <gageke@yahoo.com>
2019-04-30 16:43:59 -0400

    created p5 template for dodger game

---
+++ p5_dodger/dodger/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  // put setup code here
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.style('display', 'block');
+  background(0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // put drawing code here
+  background(0);
+ 
+}
