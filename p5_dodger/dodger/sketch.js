bbe5423d1655273c468cc9eb73c2effa6d8ac23c
Kevin Gage <gageke@yahoo.com>
2019-04-30 17:15:54 -0400

    created a guy on the canvas.  Added key presses to make him move

--- p5_dodger/dodger/sketch.js
+++ p5_dodger/dodger/sketch.js
@@ -1,16 +1,34 @@
+var guy;
+
 function setup() {
   // put setup code here
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.style('display', 'block');
   background(0);
+
+  guy = new Guy();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    guy.up();
+  }  
+}
+
 function draw() {
   // put drawing code here
   background(0);
- 
+
+  if(keyIsDown(LEFT_ARROW)){
+    guy.left();
+  } else if(keyIsDown(RIGHT_ARROW)){
+    guy.right();
+  }
+
+  guy.update();
+  guy.show();
 }
