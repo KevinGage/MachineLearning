d0d8a832119487ee72eb2986e9eba5f8c0236a27
Kevin Gage <gageke@yahoo.com>
2019-05-01 12:09:22 -0400

    added some notes about memory leak

--- p5_flappybird_ML/flappy_bird/geneticalgorithm.js
+++ p5_flappybird_ML/flappy_bird/geneticalgorithm.js
@@ -2,9 +2,17 @@ function nextGeneration() {
   console.log('next generation');
   calculateFitness();
 
+  //console.log(savedBirds.length);
   for (let i = 0; i < totalPopulation; i++) {
     birds[i] = pickOne();
-    //savedBirds[i].dispose();
+
+    //pretty sure I'm leaking memory because I am not disposing the savedBirds tensors
+    //When I try disposing them I end up crashing with "already dispose" message after a seemingly random number of iterations
+    // console.log([i]);
+    // console.log(savedBirds[i].brain);
+
+    // savedBirds[i].dispose();
+
   }
 
   pipes = [];
