7382672be91fac90b8fa585ab2fa19dff838dd25
KevinGage <dudeman02379@gmail.com>
2020-04-26 00:01:07 -0400

    experimented with graphics.  made bird render using most efficient method.  batches

--- py_flappybird/bird.py
+++ py_flappybird/bird.py
@@ -1,16 +1,31 @@
 import pyglet.app
 
 class bird():
-  def __init__(self, x, y, max_y):
+  def __init__(self, x, y, max_y, batch, size = 25 ):
     self.score = 0
     self.high_score = 0
     self.x = x
     self.y = y
     self.max_y = max_y
-    self.image = pyglet.resource.image('images/bird.png')
     self.gravity = 0.6
     self.lift = 10
     self.velocity = 0
+    self.size = size
+
+    self.vertex_list = batch.add(4, pyglet.gl.GL_QUADS, None,
+      ('v2f', self.get_corners()),
+      ('c3B', (255,255,0) * 4)
+    )
+
+    # draw using an image
+    # self.image = pyglet.resource.image('images/bird.png')
+
+    # draw using vertex list.  more efficient than manually drawing primitives
+    # but less efficient than batches
+    # self.vertex_list = pyglet.graphics.vertex_list(4,
+    #   ('v2f', corners),
+    #   ('c3B', colors)
+    # )
 
   def update(self):
     self.velocity -= self.gravity
@@ -22,13 +37,40 @@ class bird():
     
     if (self.y > self.max_y):
       self.y = self.max_y
+
+    self.vertex_list.vertices = self.get_corners()
+  
+  def get_corners(self):
+    return (self.x, self.y, self.x, self.y + self.size, self.x + self.size, self.y + self.size, self.x + self.size , self.y)
   
   def jump(self):
     self.velocity = self.lift
   
-  def draw(self):
-    self.image.blit(self.x, self.y)
-  
+  # experimented with different ways of drawing.
+  # according to the documentation the most efficient is using a batch
+  # here are other examples of drawing
+  # def draw(self):
+    # was drawing with an image
+    # self.image.blit(self.x, self.y)
+
+    # manually draw a parimitive polygon
+    # pyglet.graphics.draw(4, pyglet.gl.GL_POLYGON, 
+    #   ('v2f', (self.x, self.y, self.x, self.y + self.size, self.x + self.size, self.y + self.size, self.x + self.size , self.y)), 
+    #   ('c3B', (255, 255, 0, 255, 255, 0, 255, 255, 0, 255, 255, 0))
+    # )
+    
+    # manually draw again.  this time a quad and with cleaner syntax
+    # corners = (self.x, self.y, self.x, self.y + self.size, self.x + self.size, self.y + self.size, self.x + self.size , self.y)
+    # color_pairs = (255,255,0) * int(len(corners)/2)
+
+    # pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', corners), ('c3B', color_pairs))
+
+
+    # drawing a quad using vertex list.  More efficient than manually drawing primitives
+    # but less efficient than batches
+    # self.vertex_list.vertices = self.get_corners()
+    # self.vertex_list.draw(pyglet.gl.GL_QUADS)
+
   def setScore(self, newScore):
     self.score = newScore
     if newScore > self.high_score:
--- py_flappybird/main.py
+++ py_flappybird/main.py
@@ -4,19 +4,23 @@ from bird import bird
 
 window = pyglet.window.Window(800, 600, 'flappy bird')
 
-agent = bird(25 ,0, window.height)
+main_batch = pyglet.graphics.Batch()
+
+agent = bird(25 ,0, window.height, main_batch)
 
 score_label = pyglet.text.Label(f'Score: {agent.score}',
   font_name='Times New Roman',
   font_size=10,
   x=0, y=window.height - 20,
-  anchor_x='left', anchor_y='center')
+  anchor_x='left', anchor_y='center',
+  batch=main_batch)
 
 high_score_label = pyglet.text.Label(f'High Score: {agent.high_score}',
   font_name='Times New Roman',
   font_size=10,
   x=0, y=score_label.y - 15,
-  anchor_x='left', anchor_y='center')
+  anchor_x='left', anchor_y='center',
+  batch=main_batch)
 
 def updateScore(dt):
   if agent.y > 0:
@@ -24,10 +28,12 @@ def updateScore(dt):
   else:
     agent.setScore(0)
 
+  agent.update()
+
   score_label.text = f'Score: {agent.score}'
   high_score_label.text = f'High Score: {agent.high_score}'
 
-pyglet.clock.schedule_interval(updateScore, 1 / 60)
+pyglet.clock.schedule_interval(updateScore, 1 / 120)
 
 @window.event
 def on_key_press(symbol, modifiers):
@@ -37,9 +43,6 @@ def on_key_press(symbol, modifiers):
 @window.event
 def on_draw():
   window.clear()
-  agent.update()
-  agent.draw()
-  score_label.draw()
-  high_score_label.draw()
+  main_batch.draw()
 
 pyglet.app.run()
