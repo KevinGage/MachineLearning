49834e98461df5f08debb2812865fc7180d88252
KevinGage <dudeman02379@gmail.com>
2020-04-26 16:24:05 -0400

    added pipe logic.  added comments. used scoreboard class

--- py_flappybird/main.py
+++ py_flappybird/main.py
@@ -1,48 +1,74 @@
 import pyglet.app
+import random
 from pyglet.window import key
 from bird import bird
+from pipe import pipe
+from scoreboard import scoreboard
 
+# Setup contant variables
+PIPE_FREQUENCY = 100
+PIPE_WIDTH = 60
+PIPE_GAP = 150
+PIPE_VELOCITY = 6
+last_pipe = 0
+pipes = []
+
+
+# Create window with resolution of 800 x 600
 window = pyglet.window.Window(800, 600, 'flappy bird')
 
+# Create the main drawing batch for vertices
 main_batch = pyglet.graphics.Batch()
 
-agent = bird(25 ,0, window.height, main_batch)
+# create the player / agent at center of screen
+agent = bird(25 ,window.height / 2, window.height, main_batch)
 
-score_label = pyglet.text.Label(f'Score: {agent.score}',
-  font_name='Times New Roman',
-  font_size=10,
-  x=0, y=window.height - 20,
-  anchor_x='left', anchor_y='center',
-  batch=main_batch)
+# create scoreboard
+scoreboard = scoreboard(0, window.height - 20, main_batch)
 
-high_score_label = pyglet.text.Label(f'High Score: {agent.high_score}',
-  font_name='Times New Roman',
-  font_size=10,
-  x=0, y=score_label.y - 15,
-  anchor_x='left', anchor_y='center',
-  batch=main_batch)
-
+# logic to run every 1 / 120 seconds
 def updateScore(dt):
+  global last_pipe
+  global pipes
+
+  agent.update()
+
+  for p in pipes:
+    p.update()
+
+  pipes_off_screen = [index for index,value in enumerate(pipes) if value.x < (-1 * PIPE_WIDTH)]
+
+  for p in pipes_off_screen:
+    pipes[p].delete()
+    pipes.pop(p)
+
   if agent.y > 0:
     agent.setScore(agent.score + 1)
   else:
     agent.setScore(0)
 
-  agent.update()
+  if last_pipe % PIPE_FREQUENCY == 0:
+    rand_height = random.randint(0, window.height - PIPE_GAP)
+    new_pipe = pipe(window.width, rand_height, PIPE_WIDTH, PIPE_GAP, PIPE_VELOCITY, window.height, main_batch)
+    pipes.append(new_pipe)
+    last_pipe = 0
 
-  score_label.text = f'Score: {agent.score}'
-  high_score_label.text = f'High Score: {agent.high_score}'
+  last_pipe += 1
+  scoreboard.set_score(agent.score)
 
 pyglet.clock.schedule_interval(updateScore, 1 / 120)
 
+# handle key presses
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == key.SPACE:
         agent.jump()
 
+# do on every redraw of window
 @window.event
 def on_draw():
   window.clear()
   main_batch.draw()
 
+# run app
 pyglet.app.run()
